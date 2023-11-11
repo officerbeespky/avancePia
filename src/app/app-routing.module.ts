ded2af2079db207dc0a1308edbba61b90bd8dd16
Aldo <aldocmtz95@gmail.com>
2023-11-11 02:19:58 -0600

    avance pia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
   {
@@ -7,8 +8,12 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'feed',
+    component: FeedComponent
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'feed',
     pathMatch: 'full'
   },
 ];
